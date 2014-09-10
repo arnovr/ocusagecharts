2c2c3faecfaf5f498d719d7c397cc3f9ca5bdb2f
arnovr <arno@van-rossum.com>
2014-09-10 19:05:57 +0200

    Fixed Current storage template

--- templates/c3js/storageusagecurrentview.php
+++ templates/c3js/storageusagecurrentview.php
@@ -1,8 +1,7 @@
-<h1><?php p($l->t($_['chart']->getConfig()->getChartType())); ?></h1>
 <?php
+echo '<h1>';
+p($l->t($_['chart']->getConfig()->getChartType()));
+echo '</h1>';
 echo '<div class="chart" id="chart"><div class="icon-loading" style="height: 60px;"></div></div>';
-?>
-<?php
 $url = \OCP\Util::linkToRoute('ocusagecharts.chart_api.load_chart', array('id' => $_['chart']->getConfig()->getId(), 'requesttoken' => $_['requesttoken']));
-?>
-<div style="display: none;" data-url="<?php echo $url; ?>" id="defaultBar"></div>
+echo '<div style="display: none;" data-url="' . $url . '" id="defaultBar"></div>';
