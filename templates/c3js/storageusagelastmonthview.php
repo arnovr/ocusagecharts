a937642034611c5084f5ec6494df0b964c642007
arnovr <arno@van-rossum.com>
2014-09-10 19:13:24 +0200

    Fixed Template last month

--- templates/c3js/storageusagelastmonthview.php
+++ templates/c3js/storageusagelastmonthview.php
@@ -1,47 +1,53 @@
 <?php
 $url = \OCP\Util::linkToRoute('ocusagecharts.chart_api.load_chart', array('id' => $_['chart']->getConfig()->getId(), 'requesttoken' => $_['requesttoken']));
-?>
+echo '
 <div id="controls" style="margin-left: 3px;">
     <div id="new" class="button">
         <a
-            data-url="<?php echo $url; ?>"
+            data-url="' . $url . '"
             data-size="kb"
             data-type="line"
             data-format="%Y-%m-%d"
-            data-label="<?php p($l->t('Kilobytes')); ?>"
-            href="#"
-            >
-            <?php p($l->t('Kilobytes')); ?>
-        </a>
+            data-label="';
+p($l->t('Kilobytes'));
+echo '" href="#"
+            >';
+p($l->t('Kilobytes'));
+echo '</a>
     </div>
+
     <div id="new" class="button">
         <a
             id="defaultChart"
-            data-url="<?php echo $url; ?>"
+            data-url="' . $url . '"
             data-size="mb"
             data-type="line"
             data-format="%Y-%m-%d"
-            data-label="<?php p($l->t('Megabytes')); ?>"
-            href="#"
-            >
-            <?php p($l->t('Megabytes')); ?>
-        </a>
+            data-label="';
+p($l->t('Megabytes'));
+echo '" href="#"
+            >';
+p($l->t('Megabytes'));
+echo '</a>
     </div>
+
     <div id="new" class="button">
         <a
-            data-url="<?php echo $url; ?>"
+            data-url="' . $url . '"
             data-size="gb"
             data-type="line"
             data-format="%Y-%m-%d"
-            data-label="<?php p($l->t('Gigabytes')); ?>"
-            href="#"
-            >
-            <?php p($l->t('Gigabytes')); ?>
-        </a>
+            data-label="';
+p($l->t('Gigabytes'));
+echo '" href="#"
+            >';
+p($l->t('Gigabytes'));
+echo '</a>
     </div>
 </div>
+
 <div style="height: 50px;"></div>
-<h1><?php p($l->t($_['chart']->getConfig()->getChartType())); ?></h1>
-<?php
-echo '<div class="chart" id="chart"><div class="icon-loading" style="height: 60px;"></div></div>';
-?>
+<h1>';
+p($l->t($_['chart']->getConfig()->getChartType()));
+echo '</h1>
+<div class="chart" id="chart"><div class="icon-loading" style="height: 60px;"></div></div>';
