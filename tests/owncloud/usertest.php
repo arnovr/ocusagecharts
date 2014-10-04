9f27a70220c7d749755ee6b579bcc8da68a6db86
arnovr <arnovanrossum1@gmail.com>
2014-10-04 10:04:58 +0200

    typo

--- tests/owncloud/usertest.php
+++ tests/owncloud/usertest.php
@@ -40,18 +40,18 @@ class UserTest extends \PHPUnit_Framework_TestCase
     {
         $username = $this->user->getSignedInUsername();
         //$this->assertEquals('', $username);
-        $this->asserTrue(true);
+        $this->assertTrue(true);
     }
     public function testIsAdminUser()
     {
-        $this->asserTrue(true);
+        $this->assertTrue(true);
         //$this->assertEquals(true, $this->user->isAdminUser('admin'));
         //$this->assertEquals(false, $this->user->isAdminUser('test1'));
     }
     public function testGetSystemUsers()
     {
         $users = $this->user->getSystemUsers();
-        $this->asserTrue(true);
+        $this->assertTrue(true);
         //$this->assertContains('admin', $users);
         //$this->assertContains('test1', $users);
         //$this->assertContains('test2', $users);
