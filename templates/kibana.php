885216e623e0bd0286cce2f18602fac583c7ffe8
arnovr <arnovanrossum1@gmail.com>
2015-07-20 16:43:06 +0200

    Added dashboard url

--- templates/kibana.php
+++ templates/kibana.php
@@ -7,7 +7,14 @@ $container = $app->getContainer();
 $user = $container->query('OwncloudUser');
 $appConfig = $container->query('ServerContainer')->getConfig();
 
+$url = $appConfig->getAppValue('ocusagecharts', 'dashboard_url');
+
+if ( empty($url) )
+{
+    $url = $appConfig->getAppValue('ocusagecharts', 'dashboard_url') . ':5601/#/dashboard';
+}
+
 echo '
  <div style="width:100%; height:100%; background-color:transparent;position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px;">
-    <iframe src="' . $appConfig->getAppValue('ocusagecharts', 'url') . ':5601/#/dashboard" width="100%" height="100%" frameborder="0" style="position:absolute;width: 100%; height: 100%; border: 0px;"></iframe>
+    <iframe src="' . $url . '" width="100%" height="100%" frameborder="0" style="position:absolute;width: 100%; height: 100%; border: 0px;"></iframe>
   </div>';
