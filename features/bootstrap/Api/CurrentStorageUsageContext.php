ae4b778221c6f777e7686d6986894501ab5423d0
arnovr <arnovanrossum1@gmail.com>
2015-09-11 11:28:52 +0200

    Added default behat config and added contexts

---
+++ features/bootstrap/Api/CurrentStorageUsageContext.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Api;
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+
+class CurrentStorageUsageContext implements Context, SnippetAcceptingContext
+{
+
+}
---
+++ features/bootstrap/Domain/CurrentStorageUsageContext.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Domain;
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+
+class CurrentStorageUsageContext implements Context, SnippetAcceptingContext
+{
+
+}
